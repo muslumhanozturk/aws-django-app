3827cd92f68012c898c406fc9ac0e4709bc9a78f
Muslum Han OZTURK <121254861+muslumhanozturk@users.noreply.github.com>
2023-07-21 23:59:35 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -4,12 +4,12 @@ apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
 TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-git clone https://$TOKEN@github.com/okti3535/aws-capstone-project-de05.git
-cd /home/ubuntu/aws-capstone-project-de05
+git clone https://$TOKEN@github.com/muslumhanozturk/aws-django-app.git
+cd /home/ubuntu/aws-django-app
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/aws-capstone-project-de05/src
+cd /home/ubuntu/aws-django-app/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
